c0494638089691993a86acc8c71da9d6cee15fa4
yar-ua <yar.ua.1985@gmail.com>
2018-04-24 00:12:09 +0300

    add config

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'db' => [
+       'host' => 'localhost',
+       'user' => 'root',
+       'password' => '',
+       'db_name' => 'stud_app',
+    ],
+    'routes' => include("routes.php"),
+    'services' => include("services.php"),
+    'middleware' => include("middleware.php")
+];
