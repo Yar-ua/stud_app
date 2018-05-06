d8baab280f0d7f64d7d767ecbf9421881f879de2
yar-ua <yar.ua.1985@gmail.com>
2018-05-06 22:32:44 +0300

    fix som

--- config/services.php
+++ config/services.php
@@ -2,6 +2,5 @@
 
 return [
     'router' => 'Mindk\Framework\Routing\Router',
-    'request' => 'Mindk\Framework\Http\Request\Request',
     'Mindk\Framework\DB\DBOConnectorInterface' => 'Mindk\Framework\DB\GenericConnector',
 ];
