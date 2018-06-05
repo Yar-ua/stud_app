72228627b3067d42a3c69c2acd8fd2120ea7996a
yar-ua <yar.ua.1985@gmail.com>
2018-06-05 18:52:15 +0300

    create and delete images with base64

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -3,8 +3,10 @@
 namespace App\Controllers;
 
 use App\Models\ProductModel;
+use App\Models\ImageModel;
 use Mindk\Framework\Http\Request\Request;
 use Mindk\Framework\Exceptions\NotFoundException;
+use Mindk\Framework\Auth\AuthService;
 
 /**
  * Product controller
@@ -46,7 +48,7 @@ class ProductController
     /**
      * Create product
      */
-    function create(Request $request, ProductModel $model){
+    function create(Request $request, ProductModel $model, ImageModel $imageModel){
 
         //@TODO: Implement this
         // Accept the assumption that from the form in POST request 
@@ -56,13 +58,26 @@ class ProductController
         // just call: $request->get('storage', null, array)
 
         // Take information, what sendeed in form
+        $user = AuthService::getUser();
         $data = $request->get('formData', null, 'array');
+        $data += ['user_id' => $user->id];
+        $imageData = $request->get('image', null, '');
 
         if(empty($data)) {
             throw new \Exception('No POST data to create item');
         }
 
+        // creating product in DB
         $item = $model->create($data);
+
+        //if creating was successful
+        if ($item->id) {
+            // uploading image
+            if(!empty($imageData["filename"]) and !empty($imageData["body"])) {
+                $imageData += ['product_id' => $item->id];
+                $imageModel->upload($imageData);
+            }
+        }
 
         return $item;
     }
@@ -86,10 +101,18 @@ class ProductController
     /**
      * Delete the product
      */
-    function delete(ProductModel $model, $id) {
-        
-        $item = $model->delete($id);
-        return $item;
+    function delete(ProductModel $model, $id, ImageModel $imageModel) {
 
+        $user = AuthService::getUser();
+        $item = $model->load($id);
+        if ( ($item->user_id == $user->id) or ($user->role == 'admin') ) {
+            if ($imageModel->remove($id)) {
+                $item = $model->delete($id);
+            } else {
+                throw new \Exception('Deleting error - product not deleted');
+            }
+        }
+
+        return $item;
     }
 }
--- app/Models/ImageModel.php
+++ app/Models/ImageModel.php
@@ -15,18 +15,56 @@ class ImageModel extends Model
 {
     protected $tableName = 'images';
 
-    /**
-     * Delete record from DB
-     */
-    // public function delete($id) {
-    //     //@TODO: Implement this
-    //     // check for exsistense model in DB with specified id
-    //     if ( empty(self::load($id)) ) {
-    //         throw new \Exception('No data with current id in DB to delete');
-    //     }
+    private static $valid_types = array("gif","jpg", "png", "jpeg");
 
-    //     $sql = 'DELETE FROM `' . $this->tableName . '` WHERE product_id=' . (int)$id;
-    //     $this->dbo->setQuery($sql);
-    //     return true;
-    // }
+    /**
+     * Create image in DB and upload file
+     *
+     * @param   array $data['filename' => '...', 'body' => '...', 'product_id' => '...']
+     *
+     * @return  object
+     */
+    public function upload($data) {
+        
+        $ext = substr($data['filename'], 1 + strrpos($data['filename'], "."));
+
+        // validate correct file extendion
+        if (!in_array($ext, self::$valid_types)) {
+            throw new \Exception('Incorrect type of image, must be jpg, jpeg, png or gif');
+        }
+
+        $filename = md5(microtime()) . '.' . $ext;
+        $body = explode("base64,", $data['body']);
+
+        // if file upload was successful
+        if ( file_put_contents('uploads/' . $filename, base64_decode($body[1])) !== false ) {
+            $item = parent::create(['product_id' => $data['product_id'], 'path' => $filename]);
+            return $item;
+        } else {
+            throw new \Exception('Image not uploaded');
+        }
+    }
+
+   /**
+     * Delete image from DB and delete file
+     *
+     * @param   $id
+     *
+     * @return  object
+     */
+    public function remove($id) {
+        $sql = 'SELECT * FROM `' . $this->tableName .
+            '` WHERE `product_id`='.(int)$id;
+        $image = $this->dbo->setQuery($sql)->getResult($this);
+        if ($image) {
+            if (unlink('uploads/' . $image->path)) {
+                return parent::delete($image->id);
+            } else {
+                throw new \Exception('Image not deleted');
+            }
+        } else {
+            throw new \Exception('There is no file with current id');
+        }
+    }
+
 }
